bffce0e04fe153d9dbe9982ec92bb30c3f72ff8d
Ben Rehmann <brehmann@me.com>
2024-04-09 21:33:00 -0500

    completed expandable animation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { motion, AnimatePresence } from 'framer-motion';
+import { useState } from 'react';
+
+
+
+
 function App() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <AnimatePresence>
+      <div className="App">
+
+        <motion.div
+          className='card'
+          onClick={() => setIsOpen(!isOpen)}
+          layout
+          transition={{ layout: { duration: 1, type: 'spring' } }}
+          style={{ borderRadius: '1rem', boxShadow: '0px 10px 30px black' }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+          <motion.h2
+            layout='position'
+          >Peter Pan</motion.h2>
+
+          {isOpen && (
+            <motion.div
+              className='expand'
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1 }}
+            >
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+              </p>
+              <p>
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam
+              </p>
+            </motion.div>
+          )}
+        </motion.div>
+
+      </div>
+    </AnimatePresence>
   );
 }
 
 export default App;
+
